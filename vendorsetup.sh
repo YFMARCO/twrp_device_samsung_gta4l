f1b1ba6efadf4575971e00627da2f4c182e5c174
YFMARCO <147658205+YFMARCO@users.noreply.github.com>
2024-05-04 08:11:51 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gta4l-user
-add_lunch_combo omni_gta4l-userdebug
-add_lunch_combo omni_gta4l-eng
+add_lunch_combo twrp_gta4l-user
+add_lunch_combo twrp_gta4l-userdebug
+add_lunch_combo twrp_gta4l-eng
